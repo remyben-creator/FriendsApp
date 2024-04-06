52632a0364e6168d9670b96771e45f57afafe06c
Ben Remy <bremy@vassar.edu>
2024-04-07 01:09:11 +0800

    made it run again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
+  resources :friends 
+  #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/about'
   root 'home#index'
