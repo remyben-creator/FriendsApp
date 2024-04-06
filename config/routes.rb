a8cb0dcdc1541df40528fdc4054701f9c2f867b0
Ben Remy <bremy@vassar.edu>
2024-04-07 00:52:40 +0800

    fix delete 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'home/index'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/about'
   root 'home#index'
