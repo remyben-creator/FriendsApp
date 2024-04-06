1d9e91cd51a7de0f68751245e980d626d406c842
Ben Remy <bremy@vassar.edu>
2024-04-07 00:46:45 +0800

    fix delete 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  devise_for :users, skip: [:sessions]
-    as :user do
-      delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-    end
-  resources :friends
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/about'
